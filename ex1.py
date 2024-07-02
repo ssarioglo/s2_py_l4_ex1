a81907bdfe7e8369dffae96646b4a1e8a989f8dc
Sarioglo Sergey <swdross@gmail.com>
2024-07-02 21:14:27 +0300

    edited

---
+++ ex1.py
@@ -0,0 +1,11 @@
+
+print("Введите стороны прямоугольника")
+print("Сторона a:")
+a=float(input())
+print("Сторона b:")
+b=float(input())
+
+res = (a + b)*2
+print(f"Периметр: {res} см")
+res = a * b
+print(f"Площадь: {res} см^2")
